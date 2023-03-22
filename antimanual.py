020ad972b26f566458e110cfd863ce3028aa69ba
mounikesh24 <108253983+mounikesh24@users.noreply.github.com>
2023-03-23 00:00:24 +0530

    added date time and serial number

--- antimanual.py
+++ antimanual.py
@@ -7,14 +7,14 @@ from koto import koto
 from whiteboard import whiteboard
 from inshot import inshot
 from esfile import esfile
-from settings import settings
+# from settings import settings
 import subprocess
 
 
 subprocess.run(['adb', 'shell', 'svc', 'power', 'stayon', 'true'])
 
+name = subprocess.run(['adb', 'devices'])
 
-#
 edlp()
 print("")
 esfile()
@@ -36,9 +36,41 @@ print("")
 
 inshot()
 print("")
+
+
+cmd = ['adb', 'shell', 'date']
+
+# Run the command
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, error = process.communicate()
+
+# Decode the output
+output = output.decode().strip()
+
+# Extract the time information
+device_time = output.split(' ')[3]
+
+print('Device time:', device_time)
+
+
+# Construct the command
+cmd = ['adb', 'get-serialno']
+
+# Run the command
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, error = process.communicate()
+
+# Decode the output
+device_serial_number = output.decode().strip()
+
+print('Device serial number:', device_serial_number)
+
+#
 #
 
 #
+
+
 
 
 
--- settings.py
+++ settings.py
@@ -1,9 +1,19 @@
 from appium import webdriver
-
+import subprocess
+import time
 
 
 def settings():
 
+    # Set the package name and activity name
+    # package_name = 'com.android.settings'
+    # activity_name = 'com.android.settings.Settings$NotificationAppListActivity'
+    #
+    # # Construct the command
+    # cmd = ['adb', 'shell', 'am', 'start', '-n', f'{package_name}/{activity_name}']
+    #
+    # # Run the command
+    # subprocess.run(cmd, check=True)
     desired_caps = {
         'platformName': 'Android',
         'platformVersion': '12',
@@ -13,9 +23,27 @@ def settings():
         'noReset': True
     }
 
+    # package_name = 'com.android.settings'
+    # activity_name = '.Settings$NotificationAppListActivity'
+    #
+    # # Construct the adb command to open the app
+    # open_cmd = ['adb', 'shell', 'am', 'start', '-n', f'{package_name}/{activity_name}']
+    #
+    # subprocess.Popen(open_cmd)
+    #
+    # time.sleep(5)
+    #
+    #
+    #
+    # stop_cmd = ['adb', 'shell', 'am', 'force-stop', package_name]
+    # subprocess.Popen(stop_cmd)
+
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     # Navigate to the Notifications settings
-    driver.start_activity('com.android.settings', '.Settings$ConfigureNotificationSettingsActivity')
+    # driver.start_activity('com.android.settings', '.Settings$ConfigureNotificationSettingsActivity')
+    driver.start_activity('com.android.settings', '.Settings$NotificationAppListActivity')
 
 # Close the driver session
+
+settings()
